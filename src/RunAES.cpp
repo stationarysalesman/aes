c2cc03ac69744ff8b7ea24ef4035830af7fb4337
Tyler Camp <tyler@debian.whom>
2017-04-17 01:28:01 -0400

    working on basic acceptance tests for encryption

--- src/RunAES.cpp
+++ src/RunAES.cpp
@@ -1,6 +1,29 @@
 #include "AES.h"
+#include <iostream>
+
+/** Main program driver. */
 
 int main(int argc, char *argv[])
 {
+	if (argc != 4)
+	{
+		std::cout << "Error: " << argc-1 << " arguments given (need 3)" << std::endl;
+		return 1;
+	}
 
+	std::string keyFile = argv[2], textFile = argv[3];
+	std::string e = "e", d = "d";
+	AES cipher = AES();
+
+	/* Encrypt a file */	
+	if (e.compare(argv[1]) == 0)
+		cipher.encrypt(keyFile, textFile);
+	else if (d.compare(argv[1]) == 0)
+		cipher.decrypt(keyFile, textFile);
+	else
+	{
+		std::cout << "Error: invalid option '" << argv[1] << "'" << std::endl;
+		return 1;
+	}	
+	return 0;	
 }
